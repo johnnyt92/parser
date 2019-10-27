d61420e2fc1f955c78af0bf654ac15e2af493b1c
john T <johnnyta92@gmail.com>
2019-10-26 18:23:30 -0700

    update mongodb file and scrap.js

--- mongodb.js
+++ mongodb.js
@@ -4,10 +4,13 @@ var url = 'mongodb://localhost:27017';
 MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, (err, client) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    //...
-})
+}, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var myobj = {name:cspeaker , position: }; //Adds cspeaker from scrape.js variable
+        dbo.Collection("speakers").insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("Speaker Add");
+            db.close();
+        });
+    });
--- scrape.js
+++ scrape.js
@@ -1,20 +1,38 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var cspeaker = [] // Current speaker array
+var text = [] // What the current speaker is saying
+var speakertxt = cspeaker.concat(text) //add .toString?
+
 
 
 request('https://www.fool.com/earnings/call-transcripts/2019/10/15/johnson-johnson-jnj-q3-2019-earnings-call-transcri.aspx', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         $('p').each(function (i, element) {
-            var p = $(this).after();
-            var speaker = p.children().text();
+            var txt = $(this); // Pulls all text in P tags.
+            var speaker = txt.children().text() //Pulls speakers name in <strong> 
+            if (txt.children()) {
+                // console.log(speaker);
+                cspeaker.push(speaker); // Pushes speakers into an array
+                console.log(cspeaker)
+                // text.push(p);
+                // for (let index = 0; index < cspeaker.length; index++) {
+                //     const element = array[index]; // For loop to append what the speaker is saying to their name. Needs to stop when another <Strong> tag is found.
+                
+            } if (txt === speaker) {
+                console.log("Yes")
+            } {
+        
+                // console.log(text);
+            }
+            // var speakers = p.children().text(); //Pulls speaker's name in <strong> tags
 
             // var speaker = p.text();
             // var speaker = $(this).prev();
             // var text = $()
             // console.log(p.text());
-            // console.log(speaker);
-            console.log(speaker)
+            // console.log(speakers)
         });
     }
 });
